d211fb1bdbd76a43d61c1d1de2262766b60728df
Shubham Mahalle <shubhammahallee@gmail.com>
2026-04-26 08:55:14 +0530

    Update readConfig.py

--- utlities/readConfig.py
+++ utlities/readConfig.py
@@ -1,1 +1,23 @@
 
+import os
+import configparser
+
+config = configparser.ConfigParser()
+config.read(os.path.join(os.getcwd(), "Configuration", "config.ini"))
+
+class ReadConfig():
+    @staticmethod  # ERROR: Wrong indentation
+      def getUserurl():  # ERROR: Should be getBaseurl
+          return config.get("login", "baseurl")
+
+      @staticmethod  # ERROR: Wrong indentation
+      def getBrowser():
+          return config.get("login", "browser")
+
+      @staticmethod  # ERROR: Wrong indentation
+      def getUsername():
+          return config.get("login", "username")
+
+      @staticmethod  # ERROR: Wrong indentation
+      def getPassword():
+          return config.get("login", "password")
